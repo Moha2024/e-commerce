78bfbc9260c8b61fd916459266a87221521da6be
mohammedzhan <mohammedzhan.telzhanov.acs@gmail.com>
2026-02-11 18:10:04 +0500

    refactor: update main.go and product.go to new db logic

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,17 +1,45 @@
 package main
 
 import (
-	"e-commerce/internal/rest"
-	"net/http"
+	"e-commerce/internal/config"
+	"e-commerce/internal/database"
+	"log"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("GET /products", rest.GetProductsHandler)
-	http.HandleFunc("GET /products/{id}", rest.GetProductHandler)
-	http.HandleFunc("PUT /products/{id}", rest.UpdateProductsHandler)
-	http.HandleFunc("POST /products", rest.CreateProductHandler)
-	http.HandleFunc("DELETE /products/{id}", rest.DeleteProductHandler)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		panic(err)
+	cfg, err := config.Load()
+	if err != nil {
+		log.Fatal("Failed to load configuration:", err)
 	}
+
+	pool, err := database.InitDB(cfg.DSN)
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+	defer pool.Close()
+
+	var router *gin.Engine = gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Shop API is running",
+			"status": "success",
+			"database": "connected",
+		})
+	})
+
+
+
+	// http.HandleFunc("GET /products", rest.GetProductsHandler)
+	// http.HandleFunc("GET /products/{id}", rest.GetProductHandler)
+	// http.HandleFunc("PUT /products/{id}", rest.UpdateProductsHandler)
+	// http.HandleFunc("POST /products", rest.CreateProductHandler)
+	// http.HandleFunc("DELETE /products/{id}", rest.DeleteProductHandler)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	panic(err)
+	// }
+
+	router.Run(":" + cfg.Port)
 }
---
+++ internal/domain/models/product.go
@@ -0,0 +1,16 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Product struct {
+    ID        uuid.UUID `json:"id" db:"id"`
+    Name      string    `json:"name" db:"name"`
+    Price     float64   `json:"price" db:"price"`
+    UserID    uuid.UUID `json:"user_id" db:"user_id"`
+    CreatedAt time.Time `json:"created_at" db:"created_at"`
+    UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+}
