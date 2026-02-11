582675cba1805d15d29fca03c900eeca876c31bf
mohammedzhan <mohammedzhan.telzhanov.acs@gmail.com>
2026-02-11 18:03:16 +0500

    feat(migrations): add migrations init_schema

---
+++ migrations/000001_init_schema.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    email TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS products (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    name TEXT NOT NULL,
+    price NUMERIC(10,2) NOT NULL,
+
+    user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
