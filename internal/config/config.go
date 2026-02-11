9e27ef33d19d4532f25a11de671c0c96bb268c77
mohammedzhan <mohammedzhan.telzhanov.acs@gmail.com>
2026-02-11 18:08:17 +0500

    feat: init config, db connection and product repository

---
+++ internal/config/config.go
@@ -0,0 +1,40 @@
+package config
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Config struct {
+	DSN string
+	Port string
+}
+
+func Load() (*Config, error) {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println(".env file not found")
+	}
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_NAME"),
+	)
+
+	port := os.Getenv("SERVER_PORT")
+	if port == ""{
+		port = "8080"
+	}
+
+	return &Config{
+		DSN: dsn,
+		Port: port,
+	}, nil
+}
---
+++ internal/repository/products_repository.go
@@ -0,0 +1,36 @@
+package repository
+
+import (
+	"context"
+	"e-commerce/internal/domain/models"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func CreateProduct(pool *pgxpool.Pool, name string, price float64) (*models.Product, error){
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	query := `
+		INSERT INTO products (name, price)
+		VALUES ($1, $2)
+		RETURNING id, name, price, created_at, updated_at
+	`
+
+	var product models.Product
+
+	err := pool.QueryRow(ctx, query, name, price).Scan(
+		&product.ID,
+		&product.Name,
+		&product.Price,
+		&product.CreatedAt,
+		&product.UpdatedAt,
+	)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
